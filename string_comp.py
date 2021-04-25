6878f4915e35360ad3debfcb2cd51583262a3b83
Harsha Vardhan <gvsharsha6316@gmail.com>
2021-04-25 21:03:35 +0530

    set and hashability

--- string_comp.py
+++ string_comp.py
@@ -5,11 +5,11 @@ input = input("enter some seq: ")
 l = len(input)
 temp = ""
 counter = 1
-for i in range(1, l):
-    if input[i] == input[i-1]:
+for i in range(l):
+    if input[i] == input[i+1]:
         counter += 1
     else:
-        temp += input[i-1]+str(counter)
+        temp += input[i]+str(counter)
         counter = 1
 temp += input[i]+str(counter)
 print(temp)
